baa48fb2d102f730890a57588aa0b02f15b6b22a
Chris <christopherdchan02@gmail.com>
2024-02-09 13:15:01 -0500

    Fix training loop and implement save, load and plot losses and weights

--- image_processing/experiment.py
+++ image_processing/experiment.py
@@ -1,59 +1,58 @@
 import torch
 import pickle
+import matplotlib.pyplot as plt
 from tqdm import tqdm
 
+
 class Experiment:
-    def __init__(self, model, criterion, optimizer, device='cpu'):
+    def __init__(self, model, criterion, optimizer, device='cpu', starting_epoch=0):
         self.model = model
         self.criterion = criterion
         self.optimizer = optimizer
         self.device = device
+        self.starting_epoch = starting_epoch
 
-        self.loss = []
-        self.epoch_loss = []
-
-        self.loss_loaded = False
-        self.weights_loaded = False
+        self.loss_history = []
+        self.epoch_loss_history = []
 
     def train(self, train_loader, epochs=10):
         N = len(train_loader)
         for epoch in range(1, epochs + 1):
             self.model.train()
             epoch_loss = 0
-            with tqdm(enumerate(train_loader), total=N / train_loader.batch_size, desc=f'Epoch {epoch}/{epochs}', unit='batch') as pbar:
+            with tqdm(enumerate(train_loader), 
+                      total=N / train_loader.batch_size, 
+                      desc=f'Epoch {self.starting_epoch + epoch}/{self.starting_epoch + epochs}', 
+                      unit='batch') as pbar:
                 for i, data in pbar:
                     images, labels = data
 
                     images = images.to(self.device)
                     labels = labels.to(self.device)
 
-                    pred = self.model(images)
-
-                    loss = self.criterion(pred, labels)
-
                     self.optimizer.zero_grad()
+
+                    pred = self.model(images)
+                    loss = self.criterion(pred, labels)
                     loss.backward()
-                    torch.nn.utils.clip_grad_norm_(self.model.parameters(), 1.0)
+                    self.optimizer.step()
 
                     epoch_loss += loss.item()
-                    pbar.set_postfix(loss = loss.item())
+                    pbar.set_postfix(loss = loss.item(), avg_loss=epoch_loss / (i + 1))
+                    self.loss_history.append(loss.item())
 
-            print(f'{epoch}/{epochs} loss: {epoch_loss / N}')
-            self.epoch_loss.append(epoch_loss / N)
+            print(f'{self.starting_epoch +  epoch}/{self.starting_epoch + epochs} loss: {epoch_loss / N}')
+            self.epoch_loss_history.append(epoch_loss / N)
             epoch_loss = 0
         
-        self.loss_loaded = True
-        self.weights_loaded = True
         print("Training Complete")
 
     def save_weights(self, file_name):
-        assert self.weights_loaded
         assert file_name.ends_with(".pth")
 
         torch.save(self.model.state_dict(), file_name)
 
     def save_loss(self, file_name):
-        assert self.loss_loaded
         assert file_name.ends_with(".pkl")
 
         loss_dict = dict(loss=self.loss, epoch_loss=self.epoch_loss)
@@ -61,8 +60,12 @@ class Experiment:
         with open(file_name, 'wb') as f:
             pickle.dump(loss_dict, f)
 
-    def plot_loss():
-        pass
+    def plot_loss(self):
+        plt.plot(self.epoch_loss_history)
+        plt.title("Training Loss")
+        plt.xlabel("Epochs")
+        plt.ylabel(self.criterion.__name__)
 
     def load_model(self, file_name):
-        pass
+        state_dict = torch.load(file_name)
+        self.model.load_state_dict(state_dict)

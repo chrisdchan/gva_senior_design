299d61abba84444c45ea09461369276aaca42ff3
Christopher Chan <chrisdc@scc-q35.scc.bu.edu>
2024-03-20 17:28:26 -0400

    Fix merge conflict in gva-dataset

--- image_processing/GvaDataset.py
+++ image_processing/GvaDataset.py
@@ -22,10 +22,6 @@ class GvaDataset(Dataset):
     def __len__(self):
         return len(self.image_files)
 
-<<<<<<< Updated upstream
-=======
-
->>>>>>> Stashed changes
     def __getitem__(self, idx):
         image_path = self.image_files[idx]
         label_path = self.label_files[idx]
